4fa3cc62adfd8ef407cc15107c549fb25bb69814
nazma98 <nazmasarker1311@gmail.com>
2024-11-22 23:17:01 +0600

    Add decrease function

--- 001-counter-app/script.js
+++ 001-counter-app/script.js
@@ -4,8 +4,14 @@ let count = 0;
 const incrementElement = document.getElementById("increment");
 const decrementElement = document.getElementById("decrement");
 const counterElement = document.getElementById("counter");
+
 function increaseCount(){
     count = count + 1;
+    counterElement.innerText = count;
+}
+
+function decreaseCount(){
+    count = count - 1;
     counterElement.innerText = count;
 }
 
