b0c7cba1bf8980dacce8c296659a015bd7fd5198
nazma98 <nazmasarker1311@gmail.com>
2024-11-22 23:13:56 +0600

    Add increase function and counter element

--- 001-counter-app/script.js
+++ 001-counter-app/script.js
@@ -2,6 +2,9 @@
 let count = 0;
 
 const incrementElement = document.getElementById("increment");
-const decrementElement = document.getElementById("decrement")
+const decrementElement = document.getElementById("decrement");
+const counterElement = document.getElementById("counter");
 function increaseCount(){
+    count = count + 1;
+    counterElement.innerText = count;
 }
