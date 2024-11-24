478ede1ecd69eb044b2c0a53a2852cc19c59588f
kmtusher97 <kmtusher97@gmail.com>
2024-11-24 22:17:15 +0600

    Add a conditions for count overflow and underflow

--- 001-counter-app/script.js
+++ 001-counter-app/script.js
@@ -4,12 +4,20 @@ const incrementButton = document.getElementById('increment');
 const decrementButton = document.getElementById('decrement');
 
 function increaseCount() {
+  if (count === 10) {
+    alert('Count overflow!');
+    return;
+  }
   count++;
   counterElement.innerText = count;
 }
 
 incrementButton.addEventListener('click', increaseCount);
 decrementButton.addEventListener('click', function () {
-  count--;
-  counterElement.innerText = count;
+  if (count === 0) {
+    alert('Count will be negative');
+  } else {
+    count--;
+    counterElement.innerText = count;
+  }
 });
