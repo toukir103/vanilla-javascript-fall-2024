9097d2c27d8f9b684563d30c5706303130d7ea1d
sumiya-yasmin <sumiyayasmin665@gmail.com>
2024-11-25 19:59:40 -0300

    created new folder under the name 'sumiya-yasmin', added reset button, added the functions of decrease and reset button

--- 001-counter-app/sumiya-yasmin/script.js
+++ 001-counter-app/sumiya-yasmin/script.js
@@ -1,31 +1,22 @@
-const counterElement = document.getElementById("counter");
-const incrementButton = document.getElementById("increment");
-const decrementButton = document.getElementById("decrement");
-const resetButton = document.getElementById("reset");
-let count = 0;
-// function can be called directly
-incrementButton.addEventListener("click", function () {
-  if (count === 10) {
-    alert("The count number is overflowing");
-    return;
-  }
-  count = count + 1;
-  counterElement.innerText = count;
-});
-
-//function called separetely
-function decreaseCount() {
-  if (count === 0) {
-    alert("The count can not be negative");
-  } else {
-    count = count - 1;
-    counterElement.innerText = count;
-  }
+const counterElement= document.getElementById('counter');
+const incrementButton= document.getElementById('increment');
+const decrementButton= document.getElementById('decrement');
+const resetButton= document.getElementById('reset');
+let count=0;
+function increaseCount(){
+    count=count+1;
+    counterElement.innerText=count;
 }
-decrementButton.addEventListener("click", decreaseCount);
+incrementButton.addEventListener('click', increaseCount);
 
-function resetCount() {
-  count = 0;
-  counterElement.innerText = count;
+function decreaseCount(){
+    count=count-1;
+    counterElement.innerText=count;
 }
-resetButton.addEventListener("click", resetCount);
+decrementButton.addEventListener('click', decreaseCount);
+
+function resetCount(){
+    count=0;
+    counterElement.innerText=count;
+}
+resetButton.addEventListener('click', resetCount);
