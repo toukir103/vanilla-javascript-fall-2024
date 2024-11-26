e3a85ca4d99d9fb42bbb8445ea28efb6db01953f
nazma98 <nazmasarker1311@gmail.com>
2024-11-26 19:00:56 +0600

    Add click event listener and compare function

--- 002-number-comparator/nazma98/number-comparator.js
+++ 002-number-comparator/nazma98/number-comparator.js
@@ -2,3 +2,16 @@ let number1Element = document.getElementById("number1");
 let number2Element = document.getElementById("number2");
 let resultElement = document.getElementById("result");
 let compareBtnElement = document.getElementById("compare");
+
+compareBtnElement.addEventListener('click', function () {
+    let number1 = Number(number1Element.value);
+    let number2 = Number(number2Element.value);
+
+    if (number1 === number2) {
+        resultElement.innerText = "Both numbers are equal!";
+    } else if (number1 > number2) {
+        resultElement.innerText = "First number is greater than second number!";
+    } else {
+        resultElement.innerText = "Second number is greater than first number!";
+    }
+});
