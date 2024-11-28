18b23c14d9943b4d4201ff5dcb225774cc19638b
Armstrong104 <ameertalha22@gmail.com>
2024-11-28 18:29:12 +0600

    Added reset function and updated some code for better readability

--- 002-number-comparator/Talha/number-comparator.js
+++ 002-number-comparator/Talha/number-comparator.js
@@ -1,18 +1,29 @@
+const firstInput = document.getElementById("number1");
+const secondInput = document.getElementById("number2");
 const compareButton = document.getElementById("compare");
-const result = document.getElementById("result");
+const resultDiv = document.getElementById("result");
+const resetButton = document.getElementById("reset");
 
 compareButton.addEventListener("click", function () {
-  let firstNum = parseFloat(document.getElementById("number1").value);
-  let secondNum = parseFloat(document.getElementById("number2").value);
+  let firstNum = parseFloat(firstInput.value);
+  let secondNum = parseFloat(secondInput.value);
 
   if (isNaN(firstNum) || isNaN(secondNum)) {
-    alert("Please enter valid numbers to compare");
-    location.reload();
+    alert("Please enter valid numbers to compare!!!");
+    resetFunction();
   } else if (firstNum > secondNum) {
-    result.innerText = firstNum + " is greater than " + secondNum;
+    resultDiv.textContent = firstNum + " is greater than " + secondNum;
   } else if (firstNum < secondNum) {
-    result.innerText = firstNum + " is less than " + secondNum;
+    resultDiv.textContent = firstNum + " is less than " + secondNum;
   } else {
-    result.innerText = firstNum + " is equal to " + secondNum;
+    resultDiv.textContent = "Both numbers are equal";
   }
 });
+
+function resetFunction() {
+  firstInput.value = "0";
+  secondInput.value = "0";
+  resultDiv.textContent = "";
+}
+
+resetButton.addEventListener("click", resetFunction);
