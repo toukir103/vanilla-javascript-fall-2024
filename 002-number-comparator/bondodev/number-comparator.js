0e4a7aea98f4ce3d2b7a84c4f668722a2f2a3e00
kmtusher97 <kmtusher97@gmail.com>
2024-11-29 21:42:41 +0600

    Add comparator functions

---
+++ 002-number-comparator/bondodev/number-comparator.js
@@ -0,0 +1,17 @@
+const numberInput1 = document.getElementById('number1');
+const numberInput2 = document.getElementById('number2');
+const compareButton = document.getElementById('compare');
+const result = document.getElementById('result');
+
+compareButton.addEventListener('click', function () {
+  const number1 = parseFloat(numberInput1.value);
+  const number2 = parseFloat(numberInput2.value);
+
+  if (number1 > number2) {
+    result.innerText = 'First number is bigger';
+  } else if (number1 < number2) {
+    result.innerText = 'Second number is bigger';
+  } else {
+    result.innerText = 'Both numbers are equal';
+  }
+});
