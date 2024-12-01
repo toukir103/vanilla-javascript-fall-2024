d2a9a2fb163860ffda17ce19de405dbc0ba46667
nazma98 <nazmasarker1311@gmail.com>
2024-12-01 21:00:53 +0600

    Add reset function

--- 003-grade-calculator/nazma98/grade-calculator.js
+++ 003-grade-calculator/nazma98/grade-calculator.js
@@ -1,12 +1,38 @@
 const numberElement = document.getElementById("score");
 const calculateBtn = document.getElementById("calculate-btn");
 const result = document.getElementById("result");
-const ERROR = document.getElementById("error-message");
+const errorMsg = document.getElementById("error-message");
+const resetBtn = document.getElementById("reset-btn");
+
+const ERROR_CLASS = 'border-red-500';
+const FAIL_GRADE = 'text-red-700';
+const PASS_GRADE = 'text-green-600';
+
+function resetErrorStyles(){
+    numberElement.classList.remove(ERROR_CLASS);
+    result.classList.remove(FAIL_GRADE);
+    result.classList.remove(PASS_GRADE);
+}
+
+function isValidMark() {
+    resetErrorStyles();
+  
+    if (!numberElement.value) {
+        numberElement.classList.add(ERROR_CLASS);
+      return false;
+    }
+
+    return true;
+  }
 
 function calculateGrade(){
-    let marks = parseFloat(numberElement.value);
+    if(!isValidMark()){
+        return;
+    }
 
-    if(marks >= 80){
+    const marks = parseFloat(numberElement.value);
+    result.classList.add(PASS_GRADE);
+    if(marks >= 80 && marks <= 100){
         result.innerText = 'A+';
     } else if(marks >= 75 && marks < 80) {
         result.innerText = 'A';
@@ -24,9 +50,20 @@ function calculateGrade(){
         result.innerText = 'C';
     } else if(marks >= 40 && marks < 45) {
         result.innerText = 'D';
-    } else {
+    } else if(marks < 40 && marks >= 0) {
+        result.classList.add(FAIL_GRADE);
         result.innerText = 'F';
-    } 
+    } else {
+        errorMsg.classList.remove("hidden");
+        errorMsg.innerText = "Please enter score 0-100!"
+    }
 }
 
 calculateBtn.addEventListener("click", calculateGrade);
+
+resetBtn.addEventListener("click", function(){
+    numberElement.value = "";
+    result.innerText = "";
+    errorMsg.innerText = "";
+    resetErrorStyles();
+});
