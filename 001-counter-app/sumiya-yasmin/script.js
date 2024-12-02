554b70ffc8d2b251c47b54c8bf8283b6eba114fe
sumiya-yasmin <sumiyayasmin665@gmail.com>
2024-12-01 22:57:13 -0300

    Formate JS file

--- 001-counter-app/sumiya-yasmin/script.js
+++ 001-counter-app/sumiya-yasmin/script.js
@@ -1,32 +1,31 @@
-const counterElement= document.getElementById('counter');
-const incrementButton= document.getElementById('increment');
-const decrementButton= document.getElementById('decrement');
-const resetButton= document.getElementById('reset');
-let count=0;
+const counterElement = document.getElementById("counter");
+const incrementButton = document.getElementById("increment");
+const decrementButton = document.getElementById("decrement");
+const resetButton = document.getElementById("reset");
+let count = 0;
 // function can be called directly
-incrementButton.addEventListener('click', function(){
-    if(count===10){
-        alert('The count number is overflowing');
-        return;
-    } 
-    count=count+1;
-    counterElement.innerText=count;
+incrementButton.addEventListener("click", function () {
+  if (count === 10) {
+    alert("The count number is overflowing");
+    return;
+  }
+  count = count + 1;
+  counterElement.innerText = count;
 });
 
 //function called separetely
-function decreaseCount(){
-    if (count===0){
-        alert('The count can not be negative');
-    }
-    else{
-    count=count-1;
-    counterElement.innerText=count;
-    };
+function decreaseCount() {
+  if (count === 0) {
+    alert("The count can not be negative");
+  } else {
+    count = count - 1;
+    counterElement.innerText = count;
+  }
 }
-decrementButton.addEventListener('click', decreaseCount);
+decrementButton.addEventListener("click", decreaseCount);
 
-function resetCount(){
-    count=0;
-    counterElement.innerText=count;
+function resetCount() {
+  count = 0;
+  counterElement.innerText = count;
 }
-resetButton.addEventListener('click', resetCount);
+resetButton.addEventListener("click", resetCount);
