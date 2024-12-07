66ce11014e7238696f4adc2abaf10de711c015f7
dipuMojumder <mojumderdipu66@gmail.com>
2024-12-07 19:00:18 +0600

    added increment and decrement function

---
+++ 001-counter-app/Dipu/script.js
@@ -0,0 +1,34 @@
+let count=0;
+const counterElement=document.getElementById('counter');
+const incrementButton=document.getElementById('Increment');
+const resetButton=document.getElementById('reset');
+function increaseCount(){
+    if(count ==10)
+    {
+        alert('Overflow');
+    }
+    else{
+    count=count+1;
+    counterElement.innerText=count;
+    }
+}
+incrementButton.addEventListener('click', increaseCount);
+function decreaseCount()
+{
+    if(count==0)
+    {
+        alert('Count can not be negative');
+    }
+    else{
+    count=count-1;
+    counterElement.innerText=count;
+    }
+}
+function reset(){
+    count=0;
+    counterElement.innerText=count;
+}
+resetButton.addEventListener('click',reset)
+
+const decrementButton=document.getElementById('decrement');
+decrementButton.addEventListener('click',decreaseCount );
