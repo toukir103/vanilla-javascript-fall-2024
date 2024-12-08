a521d0cc2c11bc36689d9edfa67075d2d3229ecc
Armstrong104 <ameertalha22@gmail.com>
2024-12-08 18:53:09 +0600

    Added generateRow and reset function

--- 004-number-table/Talha/multiplication-table.js
+++ 004-number-table/Talha/multiplication-table.js
@@ -1,6 +1,7 @@
 const numberInput = document.getElementById("input-number");
 const generateBtn = document.getElementById("generate");
 const tableBody = document.getElementById("table-body");
+const resetBtn = document.getElementById("reset");
 
 generateBtn.addEventListener("click", function () {
   const num = parseInt(numberInput.value);
@@ -13,9 +14,36 @@ function cleanTable() {
   tableBody.innerHTML = "";
 }
 
-function generateTable(num){
-    for(rowNo = 1;rowNo <= 10;rowNo++){
-        const tableRow = generateRow(num,rowNo);
-        tableBody.appendChild(tableRow);
-    }
+function generateTable(num) {
+  for (let rowNo = 1; rowNo <= 10; rowNo++) {
+    const tableRow = generateRow(num, rowNo);
+    tableBody.appendChild(tableRow);
+  }
 }
+
+function generateRow(num, rowNo) {
+  const cells = [];
+  for (let i = 1; i <= 5; i++) {
+    const cell = document.createElement("td");
+    cells.push(cell);
+  }
+
+  cells[0].innerText = num;
+  cells[1].innerText = " X ";
+  cells[2].innerText = rowNo;
+  cells[3].innerText = " = ";
+  cells[4].innerText = num * rowNo;
+
+  const tableRow = document.createElement("tr");
+
+  for (let i = 0; i < cells.length; i++) {
+    tableRow.appendChild(cells[i]);
+  }
+
+  return tableRow;
+}
+
+resetBtn.addEventListener("click", function () {
+  numberInput.value = "1";
+  tableBody.innerHTML = "";
+});
