848d1346e8f7faa870a3247982ea3e755a49067f
Armstrong104 <ameertalha22@gmail.com>
2024-12-08 17:37:18 +0600

    Added cleanTable and generateTable function

--- 004-number-table/Talha/multiplication-table.js
+++ 004-number-table/Talha/multiplication-table.js
@@ -1,12 +1,21 @@
-const numberInput = document.getElementById('input-number');
-const generateBtn = document.getElementById('generate');
-const tableBody = document.getElementById('table-body');
+const numberInput = document.getElementById("input-number");
+const generateBtn = document.getElementById("generate");
+const tableBody = document.getElementById("table-body");
 
+generateBtn.addEventListener("click", function () {
+  const num = parseInt(numberInput.value);
 
-generateBtn.addEventListener('click', function(){
-    const num = parseInt(numberInput.value);
-
-    cleanTable();
-    generateTable(num);
+  cleanTable();
+  generateTable(num);
 });
 
+function cleanTable() {
+  tableBody.innerHTML = "";
+}
+
+function generateTable(num){
+    for(rowNo = 1;rowNo <= 10;rowNo++){
+        const tableRow = generateRow(num,rowNo);
+        tableBody.appendChild(tableRow);
+    }
+}
