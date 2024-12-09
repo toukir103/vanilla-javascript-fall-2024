0b8751d9a40b5c38c97397d7b87c36ff76bf3f95
Armstrong104 <ameertalha22@gmail.com>
2024-12-09 20:15:50 +0600

    Implemented all the function inside renderBtn event

--- 005-number-table-1-10/Talha/multiplication-table.js
+++ 005-number-table-1-10/Talha/multiplication-table.js
@@ -14,3 +14,45 @@ renderBtn.addEventListener("click", function () {
   }
 });
 
+function cleanTable() {
+  tableTopRow.innerHTML = "";
+  tableBottomRow.innerHTML = "";
+}
+
+function renderTable(num) {
+  const tableContainer = document.createElement("table");
+  tableContainer.className = "table-auto border border-slate-700 w-full";
+
+  const tableBody = document.createElement("tbody");
+
+  for (let row = 1; row <= 10; row++) {
+    const tableRow = renderTableRow(num, row);
+    tableBody.appendChild(tableRow);
+  }
+  tableContainer.appendChild(tableBody);
+
+  return tableContainer;
+}
+
+function renderTableRow(num, row) {
+  const cells = [];
+
+  for (let cellNo = 0; cellNo < 5; cellNo++) {
+    const cell = document.createElement("td");
+    cells.push(cell);
+  }
+
+  cells[0].innerText = num;
+  cells[1].innerText = " x ";
+  cells[2].innerText = row;
+  cells[3].innerText = " = ";
+  cells[4].innerText = num * row;
+
+  const tableRow = document.createElement("tr");
+
+  for (let cellNo = 0; cellNo < cells.length; cellNo++) {
+    tableRow.appendChild(cells[cellNo]);
+  }
+
+  return tableRow;
+}
