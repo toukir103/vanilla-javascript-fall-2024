1bf46869f6bfa89eb70a11e1a920fdec6f2744a8
sumiya-yasmin <sumiyayasmin665@gmail.com>
2024-12-08 21:52:20 -0300

    fix error text

--- 004-number-table/sumiya-yasmin/multiplication-table.js
+++ 004-number-table/sumiya-yasmin/multiplication-table.js
@@ -1,39 +1,67 @@
-const inputNumber=document.getElementById('input-number');
-const generateButton=document.getElementById('generate');
-const tableBody=document.getElementById('table-body');
+const inputNumber = document.getElementById("input-number");
+const generateButton = document.getElementById("generate");
+const tableBody = document.getElementById("table-body");
+const errorText = document.getElementById("error-text");
+const resetButton = document.getElementById("reset");
+const ERROR_CLASS = "border-red-500";
+const ERROR_TEXT = "text-red-600";
 
-generateButton.addEventListener('click', function(){
-    const num= parseInt(inputNumber.value);
+function resetErrorStyles() {
+  errorText.innerText = "";
+  inputNumber.classList.remove(ERROR_CLASS);
+}
+function isValidInput() {
+  if (!inputNumber.value) {
+    return false;
+  }
+  return true;
+}
+generateButton.addEventListener("click", function () {
+  if (!isValidInput()) {
     cleanTable();
-    generateTable(num);
+    inputNumber.classList.add(ERROR_CLASS);
+    errorText.classList.add(ERROR_TEXT);
+    errorText.innerText = "Your input is empty.Enter a valid Number";
+    return;
+  }
+  const num = parseInt(inputNumber.value);
+  cleanTable();
+  resetErrorStyles();
+  generateTable(num);
 });
 
-function cleanTable(){
-    tableBody.innerHTML='';
+function cleanTable() {
+  tableBody.innerHTML = "";
 }
-function generateTable(num){
-for(let mult=1;mult<=10;mult++){
-const tableRow= generateRow(num, mult);
-tableBody.appendChild(tableRow);
-}
+function generateTable(num) {
+  for (let mult = 1; mult <= 10; mult++) {
+    const tableRow = generateRow(num, mult);
+    tableBody.appendChild(tableRow);
+  }
+  console.log(tableBody)
 }
 
-function generateRow(num, mult){
-    const cells= [];
-    for (let i=1; i<=5; i++){
-        const cell=document.createElement('td');
-        cells.push(cell);
-    }
+function generateRow(num, mult) {
+  const cells = [];
+  for (let i = 1; i <= 5; i++) {
+    const cell = document.createElement("td");
+    cells.push(cell);
+  }
 
-    cells[0].innerText=num;
-    cells[1].innerText='x';
-    cells[2].innerText=mult;
-    cells[3].innerText='=';
-    cells[4].innerText=num*mult;
+  cells[0].innerText = num;
+  cells[1].innerText = "x";
+  cells[2].innerText = mult;
+  cells[3].innerText = "=";
+  cells[4].innerText = num * mult;
 
-    const tableRow=document.createElement('tr');
-    for(let i=0; i<cells.length; i++){
-        tableRow.appendChild(cells[i]);
-    }
-    return tableRow;
+  const tableRow = document.createElement("tr");
+  for (let i = 0; i < cells.length; i++) {
+    tableRow.appendChild(cells[i]);
+  }
+  return tableRow;
 }
+resetButton.addEventListener("click", function () {
+  inputNumber.value=''
+  tableBody.innerHTML = "";
+  resetErrorStyles();
+});
