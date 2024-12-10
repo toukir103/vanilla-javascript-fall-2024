65ea0281dcd9d34265b16fea0cfda19aa3fea181
sumiya-yasmin <sumiyayasmin665@gmail.com>
2024-12-10 20:51:24 -0300

    add table title

--- 004-number-table/sumiya-yasmin/multiplication-table.js
+++ 004-number-table/sumiya-yasmin/multiplication-table.js
@@ -2,12 +2,14 @@ const inputNumber = document.getElementById("input-number");
 const generateButton = document.getElementById("generate");
 const tableBody = document.getElementById("table-body");
 const errorText = document.getElementById("error-text");
+const titleText = document.getElementById("title");
 const resetButton = document.getElementById("reset");
 const ERROR_CLASS = "border-red-500";
 const ERROR_TEXT = "text-red-600";
 
 function resetErrorStyles() {
-  errorText.innerText = "";
+  titleText.classList.add("hidden");
+  errorText.classList.add("hidden");
   inputNumber.classList.remove(ERROR_CLASS);
 }
 function isValidInput() {
@@ -20,13 +22,19 @@ generateButton.addEventListener("click", function () {
   if (!isValidInput()) {
     cleanTable();
     inputNumber.classList.add(ERROR_CLASS);
+    errorText.classList.remove("hidden");
+    titleText.classList.add("hidden");
     errorText.classList.add(ERROR_TEXT);
-    errorText.innerText = "Your input is empty.Enter a valid Number";
+    document.getElementById("error-text");
+    errorText.innerText = "Your input is empty. Enter a valid Number";
     return;
   }
   const num = parseInt(inputNumber.value);
   cleanTable();
   resetErrorStyles();
+  titleText.classList.remove("hidden");
+  titleText.classList.add("text-bold", "text-black", "bg-green-200", "mb-2");
+  titleText.innerText = `Multiplication Table of ${inputNumber.value}`;
   generateTable(num);
 });
 
@@ -37,8 +45,8 @@ function generateTable(num) {
   for (let mult = 1; mult <= 10; mult++) {
     const tableRow = generateRow(num, mult);
     tableBody.appendChild(tableRow);
+    tableBody.classList.add("bg-gray-100");
   }
-  console.log(tableBody)
 }
 
 function generateRow(num, mult) {
@@ -49,7 +57,7 @@ function generateRow(num, mult) {
   }
 
   cells[0].innerText = num;
-  cells[1].innerText = "x";
+  cells[1].innerText = " x ";
   cells[2].innerText = mult;
   cells[3].innerText = "=";
   cells[4].innerText = num * mult;
@@ -61,7 +69,7 @@ function generateRow(num, mult) {
   return tableRow;
 }
 resetButton.addEventListener("click", function () {
-  inputNumber.value=''
+  inputNumber.value = "";
   tableBody.innerHTML = "";
   resetErrorStyles();
 });
