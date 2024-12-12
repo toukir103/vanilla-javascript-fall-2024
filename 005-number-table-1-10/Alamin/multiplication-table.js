79e10deb5c5dafa305b9bdea48df1aa44627faa2
Alamin374 <aaalaamin7@gmail.com>
2024-12-13 00:25:11 +0600

    Added number multiplication table from 1-10

--- 005-number-table-1-10/Alamin/multiplication-table.js
+++ 005-number-table-1-10/Alamin/multiplication-table.js
@@ -1,6 +1,7 @@
 const tableContainerTopRow = document.getElementById('top-row');
 const tableContainerBottomRow = document.getElementById('bottom-row');
 const generateBtn = document.getElementById('generate');
+const resetBtn = document.getElementById('reset-btn'); // Reset button
 
 generateBtn.addEventListener('click', function () {
   for (let num = 1; num <= 10; num++) {
@@ -13,18 +14,47 @@ generateBtn.addEventListener('click', function () {
     }
   }
 });
+
+resetBtn.addEventListener('click', function () {
+  // Clear the content of both rows
+  tableContainerTopRow.innerHTML = '';
+  tableContainerBottomRow.innerHTML = '';
+});
+
 function getTable(num) {
-    const numberTable = document.createElement('table');
-    numberTable.className = 'table-auto border border-slate-700 w-full';
-  
-    const tableBody = document.createElement('tbody');
-  
-    for (let row = 1; row <= 10; row++) {
-      const tableRow = getTableRow(num, row);
-      tableBody.appendChild(tableRow);
-    }
-  
-    numberTable.appendChild(tableBody);
-  
-    return numberTable;
+  const numberTable = document.createElement('table');
+  numberTable.className = 'table-auto border border-slate-700 w-full';
+
+  const tableBody = document.createElement('tbody');
+
+  for (let row = 1; row <= 10; row++) {
+    const tableRow = getTableRow(num, row);
+    tableBody.appendChild(tableRow);
   }
+
+  numberTable.appendChild(tableBody);
+
+  return numberTable;
+}
+
+function getTableRow(num, row) {
+  const cells = [];
+  for (let cellNo = 0; cellNo < 5; cellNo++) {
+    const cell = document.createElement('td');
+    cells.push(cell);
+  }
+
+  cells[0].innerText = num;
+  cells[1].innerText = ' x ';
+  cells[2].innerText = row;
+  cells[3].innerText = ' = ';
+  cells[4].innerText = num * row;
+
+  const tableRow = document.createElement('tr');
+
+  for (let cellNo = 0; cellNo < cells.length; cellNo++) {
+    tableRow.appendChild(cells[cellNo]);
+  }
+
+  return tableRow;
+}
